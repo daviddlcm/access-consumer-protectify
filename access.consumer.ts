e70fc27b2c6871b6918e52c40d7805d87c5e980e
david <daviddelacruzmorales42@gmail.com>
2024-07-18 13:32:19 -0600

    my first commit

---
+++ access.consumer.ts
@@ -0,0 +1,72 @@
+import mqtt from 'mqtt';
+const socketIoClient = require('socket.io-client');
+import { Socket } from 'socket.io-client';
+
+const USERNAME = "protectify";
+const PASSWORD = "adminadmin"; 
+const HOSTNAME = "54.144.149.49";
+const PORT = 1883;
+const MQTT_TOPIC = "esp32";
+const WEBSOCKET_SERVER_URL = "ws://localhost:80";
+
+let socketIO: Socket;
+let isRoomJoined = false; 
+
+async function connect() {
+  try {
+    const client = mqtt.connect(`mqtt://${USERNAME}:${PASSWORD}@${HOSTNAME}:${PORT}`);
+
+    client.on('connect', () => {
+      console.log('Connected to MQTT broker');
+      client.subscribe(MQTT_TOPIC, (err) => {
+        if (err) {
+          console.error('Error subscribing to topic:', err.message);
+        }
+      });
+    });
+
+    client.on('error', (err) => {
+      console.error('MQTT connection error:', err.message);
+    });
+
+    client.on('message', (topic, message) => {
+      if (topic === MQTT_TOPIC) {
+        const parsedContent = JSON.parse(message.toString());
+        console.log('accessData:', parsedContent);
+
+        const userId = parsedContent.id;
+        
+        if (!isRoomJoined) {
+          socketIO.emit('joinRoom', userId);
+          isRoomJoined = true;
+        }
+        
+        socketIO.emit('event-access', parsedContent);
+      }
+    });
+
+    socketIO = socketIoClient(WEBSOCKET_SERVER_URL, {
+      transports: ['websocket'],
+      path: '/socket.io'
+    });
+
+    socketIO.on('connect', () => {
+      console.log('Connected to WebSocket server');
+    });
+
+    socketIO.on('connect_error', (err: any) => {
+      console.error('WebSocket connection error:', err.message);
+    });
+
+    socketIO.on('disconnect', (reason: any) => {
+      console.error('WebSocket disconnected:', reason);
+      isRoomJoined = false; 
+    });
+
+  } catch (err: any) {
+    console.error('Error during connection setup:', err.message);
+    throw new Error(err);
+  }
+}
+
+connect();
